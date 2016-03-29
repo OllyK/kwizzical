1c98ff59be0a9eeb4a94ffba54e74d8b9fc87121
Olly King <ok15351@my.bristol.ac.uk>
2016-03-29 16:25:04 +0100

    Animates on refresh

--- js/kwizecal.js
+++ js/kwizecal.js
@@ -4,28 +4,7 @@
 var kwizecalApp = angular
 .module('kwizecalApp', ['ngRoute'])
 .controller('mainController', runQuiz)
-.controller('MyCtrl', [function() {
-  angular.element(document).ready(function () {
-   for (var i = 0; i < 2; i++) {
-     var path = document.querySelector('#i' + i);
-     var length = path.getTotalLength();
-
-     path.style.transition = path.style.WebkitTransition = path.style.MozTransition = 'none';
-
-     path.style.strokeDasharray = length + ' ' + length;
-     path.style.strokeDashoffset = length ;
-
-     path.getBoundingClientRect();
-
-     path.style.transition = path.style.WebkitTransition = path.style.MozTransition = 'stroke-dashoffset 3s ease-in-out';
-
-    // GO !
-
-    path.style.strokeDashoffset = '0';
-    
-  }
-});
-}])
+.controller('MyCtrl', runAnimation)
 .config(routing)
 .directive("header", header)
 .directive("footer", footer)
@@ -64,6 +43,29 @@ function quiz() {
     transclude : false,
     controller: 'mainController'
   };
+}
+
+function runAnimation($scope){
+  $scope.$on('$includeContentLoaded',function () {
+   for (var i = 0; i < 2; i++) {
+     var path = document.querySelector('#i' + i);
+     var length = path.getTotalLength();
+
+     path.style.transition = path.style.WebkitTransition = path.style.MozTransition = 'none';
+
+     path.style.strokeDasharray = length + ' ' + length;
+     path.style.strokeDashoffset = length ;
+
+     path.getBoundingClientRect();
+
+     path.style.transition = path.style.WebkitTransition = path.style.MozTransition = 'stroke-dashoffset 3s ease-in-out';
+
+    // GO !
+
+    path.style.strokeDashoffset = '0';
+    
+  }
+});
 }
 
 function runQuiz($scope, $http, $log) {
