cf00f375aae32d167893965791d6d781405b0c32
gg15330 <gg15330@my.bristol.ac.uk>
2016-05-19 15:26:38 +0100

    clicking on a quiz changes the url to the id of the quiz

--- js/quizzes.js
+++ js/quizzes.js
@@ -18,35 +18,4 @@ function qlCtrl($scope, $http) {
   function showError(reason) {
     console.log(reason);
   }
-
-  //initialises quiz variables
-  $scope.init = function() {
-
-  }
-
-  //checks answer and updates score, ends quiz if last question is reached
-  // $scope.update = function(choice) {
-  //   if(!$scope.choicemade) {
-  //     if(choice == $scope.quiz.questions[$scope.i].answer) { $scope.score++; }
-  //     if($scope.i >= $scope.quiz.questions.length - 1) {
-  //       $scope.running = false;
-  //       $scope.msg = "Final score: ";
-  //       $scope.btnmsg = "Restart";
-  //     }
-  //     $scope.choicemade = true;
-  //   }
-  // }
-
-  //moves on to next question -
-  //if end of quiz, resets quiz
-  // $scope.next = function() {
-  //   if($scope.running) {
-  //     $scope.i++;
-  //     $scope.choicemade = false;
-  //   }
-  //   else { $scope.init(); }
-  // }
-  //
-  // //call to init displays start of quiz
-  // $scope.init();
 }
